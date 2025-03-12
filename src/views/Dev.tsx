d9098afd798c5606a866749c0e0efa07af1ab984
amitlzkpa <amit.lzkpa@gmail.com>
2025-03-12 22:02:55 +0530

    Paste and download file

--- src/views/Dev.tsx
+++ src/views/Dev.tsx
@@ -20,28 +20,7 @@ import {
 } from "convex/react";
 import { Id } from "../../convex/_generated/dataModel";
 import { api } from "../../convex/_generated/api";
-
-export function downloadBase64Response(text: string, filename: string) {
-  // const base64String = Buffer.from(text).toString('base64');
-  const byteCharacters = atob(text);
-  const byteNumbers = new Array(byteCharacters.length);
-
-  for (let i = 0; i < byteCharacters.length; i++) {
-    byteNumbers[i] = byteCharacters.charCodeAt(i);
-  }
-
-  const byteArray = new Uint8Array(byteNumbers);
-  const blob = new Blob([byteArray]);
-  const url = URL.createObjectURL(blob);
-
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = filename;
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-  URL.revokeObjectURL(url);
-};
+import { base64StringToBlob } from "../utils";
 
 export default function Dev() {
   const cvxUtils = useCvxUtils();
@@ -49,7 +28,15 @@ export default function Dev() {
   const [text, setText] = useState("");
 
   const handleTestButtonClick = async () => {
-    downloadBase64Response(text, "data.jpg");
+    const filename = "test.jpg";
+    const blob = base64StringToBlob(text, filename);
+
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob!);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
   return (
