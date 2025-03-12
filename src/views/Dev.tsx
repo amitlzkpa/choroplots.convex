3c235ae84a00de9cdabd7c444bc254c149ce92cc
amitlzkpa <amit.lzkpa@gmail.com>
2025-03-12 21:32:42 +0530

    Update downloadBase64Response to match usage

--- src/views/Dev.tsx
+++ src/views/Dev.tsx
@@ -21,28 +21,26 @@ import {
 import { Id } from "../../convex/_generated/dataModel";
 import { api } from "../../convex/_generated/api";
 
-export function downloadBase64Response(response: Response, filename: string) {
-  response.text().then((text) => {
-    const base64String = Buffer.from(text).toString('base64');
-    const byteCharacters = atob(base64String);
-    const byteNumbers = new Array(byteCharacters.length);
+export function downloadBase64Response(text: string, filename: string) {
+  // const base64String = Buffer.from(text).toString('base64');
+  const byteCharacters = atob(text);
+  const byteNumbers = new Array(byteCharacters.length);
 
-    for (let i = 0; i < byteCharacters.length; i++) {
-      byteNumbers[i] = byteCharacters.charCodeAt(i);
-    }
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
 
-    const byteArray = new Uint8Array(byteNumbers);
-    const blob = new Blob([byteArray]);
-    const url = URL.createObjectURL(blob);
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray]);
+  const url = URL.createObjectURL(blob);
 
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-  }).catch(console.error);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
 };
 
 export default function Dev() {
