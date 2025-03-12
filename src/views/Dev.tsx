7233980c1255d16de749c77234eef57b8a9894f0
amitlzkpa <amit.lzkpa@gmail.com>
2025-03-12 21:27:01 +0530

    Add function to downloadBase64Response

--- src/views/Dev.tsx
+++ src/views/Dev.tsx
@@ -21,6 +21,30 @@ import {
 import { Id } from "../../convex/_generated/dataModel";
 import { api } from "../../convex/_generated/api";
 
+export function downloadBase64Response(response: Response, filename: string) {
+  response.text().then((text) => {
+    const base64String = Buffer.from(text).toString('base64');
+    const byteCharacters = atob(base64String);
+    const byteNumbers = new Array(byteCharacters.length);
+
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray]);
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }).catch(console.error);
+};
+
 export default function Dev() {
   const cvxUtils = useCvxUtils();
 
