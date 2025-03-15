96eec9e38bb8b9945a3fa017caa51792b7d5ebdc
amitlzkpa <amit.lzkpa@gmail.com>
2025-03-15 11:03:24 +0530

    Enabel cors from http side

--- convex/vsHttpActions.ts
+++ convex/vsHttpActions.ts
@@ -6,6 +6,18 @@ import { v } from "convex/values";
 
 export const mapCreate = httpAction(async (ctx, request) => {
 
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  }
+
+
   const { text } = await request.json();
 
   console.log(text);
